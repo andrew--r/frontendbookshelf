7c7174b4ee002bc9cca755595e426eb3085d54a1
Andrew Romanov <me@andrew-r.ru>
2017-01-22 01:01:23 +0300

    Remove gulp-if in favor of ternary condition with gutil.noop()

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,18 +1,17 @@
 import gulp from 'gulp';
+import gutil from 'gulp-util';
 import webpackStream from 'webpack-stream';
 import uglify from 'gulp-uglify';
-import gulpif from 'gulp-if';
 import { PATHS } from '../config';
 import { getPluginOptions } from '../helpers';
 
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 gulp.task('scripts', () => {
 	return gulp
 		.src(`${PATHS.source.scripts}/index.js`)
 		.pipe(webpackStream(getPluginOptions('webpackStream')))
-		.pipe(gulpif(
-			process.env.NODE_ENV === 'production',
-			uglify(getPluginOptions('uglify'))
-		))
+		.pipe(isProduction ? uglify(getPluginOptions('uglify')) : gutil.noop())
 		.pipe(gulp.dest(PATHS.build.scripts));
 });
